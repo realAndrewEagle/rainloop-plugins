d15416bb9d42a2f41372dd621f691ea3bf5ef934
TheBestIsaac <andreas.adler@aqsw.org>
2014-12-17 00:10:16 +0100

    Updated indentation in function for uberspace domain checking

--- uberspace-change-password/UberspaceChangePasswordDriver.php
+++ uberspace-change-password/UberspaceChangePasswordDriver.php
@@ -62,7 +62,7 @@ class UberspaceChangePasswordDriver implements \RainLoop\Providers\ChangePasswor
 	private function IsDirectUberspaceDomain($domainName)
 	{
 		return $this->EndsWith(strtolower($domainName, ".uberspace.de")) &&
-			   $this->IsInRange(count(explode(".", $domainName)), 3, 4);
+			$this->IsInRange(count(explode(".", $domainName)), 3, 4);
 	}
 
 	private function DomainIncludesUberspaceAccountName($domainName)
